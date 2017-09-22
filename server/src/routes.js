25c2eb7ca6c466910377b982876dbf3183ba0346
Noval Parinussa <novalparinussa@gmail.com>
2017-09-22 23:54:47 +0000

    Connecting to SQLite using Sequelize and freshening up the register page

---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
+const AuthenticationControllerPolicies = require('./policies/AuthenticationControllerPolicies')
+
+module.exports = (app) => {
+  app.post('/register',
+    AuthenticationControllerPolicies.register,
+    AuthenticationController.register)
+}
